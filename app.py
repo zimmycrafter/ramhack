ed81f1b8bd028a5744d5c5877cf2243687e0fb3b
zimmycrafter <zimmycrafterplaysminecraftpepc@gmail.com>
2023-01-28 18:11:38 -0500

    py

--- app.py
+++ app.py
@@ -15,11 +15,11 @@ model.eval()
 
 def transform_image(image_bytes):
     my_transforms = transforms.Compose([transforms.Resize(255),
-                                        transforms.CenterCrop(224),
-                                        transforms.ToTensor(),
-                                        transforms.Normalize(
-                                            [0.485, 0.456, 0.406],
-                                            [0.229, 0.224, 0.225])])
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(
+            [0.485, 0.456, 0.406],
+            [0.229, 0.224, 0.225])])
     image = Image.open(io.BytesIO(image_bytes))
     return my_transforms(image).unsqueeze(0)
 
