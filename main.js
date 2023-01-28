67fefee2e71b547c3aaf13996b936cc28f9f5051
zimmycrafter <zimmycrafterplaysminecraftpepc@gmail.com>
2023-01-28 17:16:12 -0500

    edits

--- main.js
+++ main.js
@@ -154,3 +154,29 @@ function changePage(inp) {
         }
     });
 }
+
+function submitPicture() {
+// (Can't use `typeof FileReader === "function"` because apparently it
+// comes back as "object" on some browsers. So just see if it's there
+// at all.)
+if (!window.FileReader) { // This is VERY unlikely, browser support is near-universal
+  console.log("The file API isn't supported on this browser yet.");
+  return;
+}
+
+var input = document.getElementById('fileinput');
+if (!input.files) { // This is VERY unlikely, browser support is near-universal
+  console.error("This browser doesn't seem to support the `files` property of file inputs.");
+} else if (!input.files[0]) {
+  addPara("Please select a file before clicking 'Load'");
+} else {
+  var file = input.files[0];
+  addPara(`<img src="${file.val}">`)
+}
+}
+
+function addPara(text) {
+  var p = document.createElement("p");
+  p.textContent = text;
+  document.body.appendChild(p);
+}
